8a65bd9225b1ee301598198dad1ecd5a146bd5f9
harishviswa <78377213+harishviswa@users.noreply.github.com>
2021-12-01 13:48:43 -0500

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,26 @@
+// ITCS
+// 02.23.2021
+// Harish Viswa
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		
+		int number = 100;
+		for(int i=1; i<=number;i++) {
+			if(i%3 == 0) {
+				System.out.println("Fizz");
+			}
+			else if(i%5 == 0) {
+				System.out.println("Fizz");
+			} else {
+			System.out.println(i);
+			}
+		}
+		
+
+	}
+		
+	}
+	
+
